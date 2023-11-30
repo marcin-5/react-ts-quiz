6d970c350127b5e3037417cad3ade16f85ef0a79
Marcin Bojara <marcin_5@mail.ru>
2023-11-30 22:29:58 +0100

    add lottie animations

--- src/features/PlayQuiz.tsx
+++ src/features/PlayQuiz.tsx
@@ -1,11 +1,24 @@
 import { Flex, Heading, Radio, RadioGroup, SimpleGrid, Text } from "@chakra-ui/react";
-import { useState } from "react";
+import Lottie from "lottie-react";
+import { useEffect, useState } from "react";
+import invalidAnimation from "../assets/lottie/invalid.json";
+import validAnimation from "../assets/lottie/valid.json";
 import { QuizItem } from "../types/quiz-type";
 
 export function PlayQuiz(p: { quiz: QuizItem[] }) {
   const [currentQuizItemIndex, setCurrentQuizItemIndex] = useState<number>(0);
+  const [answer, setAnswer] = useState<string>();
+  const [questionStatus, setQuestionStatus] = useState<"valid" | "invalid" | "unanswered">("unanswered");
   const currentQuizItem: QuizItem = p.quiz[currentQuizItemIndex];
   const availableAnswers: string[] = [currentQuizItem.correct_answer, ...currentQuizItem.incorrect_answers];
+
+  const isValidAnswer = (answer: string): boolean => answer === currentQuizItem.correct_answer;
+  
+  useEffect(() => {
+    if (answer) {
+      setQuestionStatus(isValidAnswer(answer) ? "valid" : "invalid");
+    }
+  }, [answer]);
 
   const radioList = availableAnswers.map((answer: string, i: number) => {
     return (
@@ -16,13 +29,24 @@ export function PlayQuiz(p: { quiz: QuizItem[] }) {
   });
 
   return (
-    <Flex direction={"column"} alignItems={"center"} justify={"center"}>
+    <Flex direction={"column"} alignItems={"center"} justify={"center"} px={10}>
       <Heading fontSize={"3xl"} mt={100} mb={20} dangerouslySetInnerHTML={{ __html: currentQuizItem.question }} />
-      <RadioGroup value={""} onChange={() => setCurrentQuizItemIndex(currentQuizItemIndex + 1)}>
+      <RadioGroup value={answer} onChange={setAnswer}>
         <SimpleGrid columns={2} spacing={4}>
           {radioList}
         </SimpleGrid>
       </RadioGroup>
+      <Lottie
+        loop={false}
+        style={{ marginTop: 100, height: 150 }}
+        animationData={
+          questionStatus === "unanswered" ? null : questionStatus === "valid" ? validAnimation : invalidAnimation
+        }
+        onComplete={() => {
+          setQuestionStatus("unanswered");
+          setCurrentQuizItemIndex(currentQuizItemIndex + 1);
+        }}
+      />
     </Flex>
   );
 }
