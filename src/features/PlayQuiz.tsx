00e1efe267478f4bd158818d961c7aa9b39d943b
Marcin Bojara <marcin_5@mail.ru>
2023-11-30 22:47:45 +0100

    add colorize answers

--- src/features/PlayQuiz.tsx
+++ src/features/PlayQuiz.tsx
@@ -9,21 +9,30 @@ export function PlayQuiz(p: { quiz: QuizItem[] }) {
   const [currentQuizItemIndex, setCurrentQuizItemIndex] = useState<number>(0);
   const [answer, setAnswer] = useState<string>();
   const [questionStatus, setQuestionStatus] = useState<"valid" | "invalid" | "unanswered">("unanswered");
+  const [availableAnswers, setAvailableAnswers] = useState<string[]>([]);
   const currentQuizItem: QuizItem = p.quiz[currentQuizItemIndex];
-  const availableAnswers: string[] = [currentQuizItem.correct_answer, ...currentQuizItem.incorrect_answers];
 
   const isValidAnswer = (answer: string): boolean => answer === currentQuizItem.correct_answer;
-  
+
   useEffect(() => {
     if (answer) {
       setQuestionStatus(isValidAnswer(answer) ? "valid" : "invalid");
     }
   }, [answer]);
 
+  useEffect(() => {
+    setAvailableAnswers(
+      [currentQuizItem.correct_answer, ...currentQuizItem.incorrect_answers]
+    );
+  }, [currentQuizItemIndex]);
+
   const radioList = availableAnswers.map((answer: string, i: number) => {
     return (
       <Radio key={i} value={answer}>
-        <Text dangerouslySetInnerHTML={{ __html: answer }} />
+        <Text
+          color={questionStatus === "unanswered" ? "black" : isValidAnswer(answer) ? "green.400" : "red.400"}
+          dangerouslySetInnerHTML={{ __html: answer }}
+        />
       </Radio>
     );
   });
@@ -31,7 +40,7 @@ export function PlayQuiz(p: { quiz: QuizItem[] }) {
   return (
     <Flex direction={"column"} alignItems={"center"} justify={"center"} px={10}>
       <Heading fontSize={"3xl"} mt={100} mb={20} dangerouslySetInnerHTML={{ __html: currentQuizItem.question }} />
-      <RadioGroup value={answer} onChange={setAnswer}>
+      <RadioGroup value={answer} onChange={questionStatus === "unanswered" ? setAnswer : undefined}>
         <SimpleGrid columns={2} spacing={4}>
           {radioList}
         </SimpleGrid>
